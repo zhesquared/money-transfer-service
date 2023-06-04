d1bb5c10b11b91e9551496ac3e7512cd6acbd84d
Eugene Borisov <eugeni.borisov@gmail.com>
2023-06-04 13:03:14 +0300

    remove comments

--- src/main/java/ru/netology/moneytransferservice/servise/CardDataValidation.java
+++ src/main/java/ru/netology/moneytransferservice/servise/CardDataValidation.java
@@ -5,12 +5,12 @@ import ru.netology.moneytransferservice.domain.Transfer;
 
 public interface CardDataValidation {
 
-    boolean validTillValidation(Transfer transfer, Card validCardFrom);// throws InvalidCardDataException;
+    boolean validTillValidation(Transfer transfer, Card validCardFrom);
 
-    boolean CVVValidation(Transfer transfer, Card validCardFrom);// throws InvalidCardDataException;
+    boolean CVVValidation(Transfer transfer, Card validCardFrom);
 
-    boolean transferCurrencyValidation(Transfer transfer, Card validCardFrom);// throws InvalidCardDataException;
+    boolean transferCurrencyValidation(Transfer transfer, Card validCardFrom);
 
-    boolean cardAvailableAmountValidation(Transfer transfer, Card validCardFrom, Double transferCommission);// throws InvalidCardDataException;
+    boolean cardAvailableAmountValidation(Transfer transfer, Card validCardFrom, Double transferCommission);
 
 }
